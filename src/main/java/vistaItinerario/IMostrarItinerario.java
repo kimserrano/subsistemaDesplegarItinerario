22bead364e946786253b8e4435cacf54b6f18e97
azzertijo <azzertijo@outlook.com>
2023-05-17 23:04:32 -0700

    Fachada de desplegar itineario
    fachada

---
+++ src/main/java/vistaItinerario/IMostrarItinerario.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package vistaItinerario;
+
+import ObjNegocio.Itinerario;
+
+/**
+ *
+ * @author JORGE
+ */
+public interface IMostrarItinerario {
+    
+   public void reporteItinerario(Itinerario itinerario);
+   
+}
